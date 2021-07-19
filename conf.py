4eacc63b255f8c5bef2ec135afefc1fc44966b13
Антон Шушменцев <shusmencev97@mail.ru>
2021-07-19 15:14:48 +0600

    Добавил новую версию драйвера для Хрома

--- conf.py
+++ conf.py
@@ -6,5 +6,5 @@ from pathlib import Path
 DIR_SRC = Path(__file__).parent.resolve()
 
 # Пути к драйверам:
-PTH_CHROME_DRV_WIN = DIR_SRC / "bin" / "brw" / "drv" / "chrome" / "w_drv_87.exe"
+PTH_CHROME_DRV_WIN = DIR_SRC / "bin" / "brw" / "drv" / "chrome" / "w_drv_91.exe"
 PTH_CHROMIUM_DRV_LIN = DIR_SRC / "bin" / "brw" / "drv" / "chromium" / "l_drv_83"
